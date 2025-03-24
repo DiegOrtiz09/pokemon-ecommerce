4bc87d455a6988004f1de698409ff6ed2cd81f9a
DiegOrtiz09 <st3439@utr.edu.mx>
2025-03-24 00:35:27 -0600

    Were removed and added some elements

--- frontpoke/src/components/Header.js
+++ frontpoke/src/components/Header.js
@@ -8,22 +8,12 @@ const Header = (setIsAuthenticated) => {
     <header style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
       <h1>PokéVerse</h1>
 
-      {/*Search Bar*/}
-      <div style={{ display: 'flex', alignItems: 'center', backgroundColor: 'white', padding: '5px', borderRadius: '5px' }}>
-        <FaSearch style={{ marginRight: '5px', color: 'gray' }} />
-        <input 
-          type="text" 
-          placeholder="Buscar Pokémon..." 
-          style={{ border: 'none', outline: 'none', padding: '5px', width: '400px' }} 
-        />
-      </div>
-
       <nav>
         <a href="/home" style={{ marginRight: '15px', textDecoration: 'none', color: 'black' }}>
         <FaHome size={20} /> Home</a>
         <a href='/login' style={{ marginRight: '15px', textDecoration: 'none', color: 'black' }}>
         <FaSignOutAlt size={20} /> LogOut</a>
-        <a href="/favorites" style={{ marginRight: '15px', textDecoration: 'none', color: 'black' }}>
+        <a href="/favorite" style={{ marginRight: '15px', textDecoration: 'none', color: 'black' }}>
           <FaHeart size={30} /></a>
         <a href="/cart" style={{ marginRight: '15px', textDecoration: 'none', color: 'black' }}>
           <FaShoppingCart size={30} /></a>
